efd5bdca3f7376fed58277566fa111fe303c7b78
ADogTheDestroyer <andrewhlin0@gmail.com>
2022-12-18 08:28:49 -0500

    changed 'Submit' button to 'Draw'

--- src/index.js
+++ src/index.js
@@ -117,7 +117,7 @@ class NumDotsForm extends React.Component {
           Enter the number of dots to be draw: 
           <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Draw" />
       </form>
     );
   }
