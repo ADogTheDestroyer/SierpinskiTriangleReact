15a6e4a3ac9c4560a4e720a3862fe911ceef201c
ADogTheDestroyer <andrewhlin0@gmail.com>
2022-12-19 23:48:45 -0500

    encapsulated triangle code into its own component

--- src/index.js
+++ src/index.js
@@ -6,95 +6,105 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // renders everything in a single root element
 function renderAll(quantity) {
-  let key = 0;
   root.render(
     <div>
-      <div id="formContainer">
-        <NumDotsForm/>
-      </div>
-      
-      {
-        generateDots(quantity).map(dot => 
-          <div key={"dot" + key++}>{dot}</div> // React requires that each element of a list has a unique key
-        )
-      }
+      <NumDotsForm/>
+      <Triangle quantity={quantity}/>
     </div>
-  );
+  )
 }
 
-// checks if a point is within the area of the triangle
-function pointIsValid(xPos, yPos) {
-  const fr = (-33/25) * xPos + 132; //function of the right side of the triangle
-  const fl = (33/25) * xPos;        //function of the left side of the triangle
+// This is the triangle itself, all of its methods 
+class Triangle extends React.Component {
 
-  if(xPos >= 50) { //check which side of the triangle the point is on
-    return yPos < fr;
-  } else {
-    return yPos < fl;
-  }
-}
+    // checks if a point is within the area of the triangle
+  pointIsValid(xPos, yPos) {
+    const fr = (-33/25) * xPos + 132; //function of the right side of the triangle
+    const fl = (33/25) * xPos;        //function of the left side of the triangle
 
-// generates the a random point within the area of the triangle
-function calculateFirstPoint() {
-  let xPos;
-  let yPos;
-  do {
-    xPos = Math.floor(Math.random() * 50) + 25;
-    yPos = Math.floor(Math.random() * 33) + 33;
-  } while (!pointIsValid(xPos, yPos));
-
-  return [xPos, yPos];
-}
-
-// calculates the next point to plot, ie the mid point between a randomly
-// chosen corner point, and the previous plotted point
-function calculateNextPoint(point) {
-  let xPos = point[0];
-  let yPos = point[1];
-
-  // picks one of the three corners at random
-  let trianglePoint = Math.floor(Math.random() * 3) + 1;
-
-  // returns the midpoint between 'point' and the randomly chosen corner point
-  switch(trianglePoint) {
-    case 1: // top corner @(50, 66)
-      return [(50+xPos)/2, (66+yPos)/2];
-
-    case 2: // right bottom corner @(75, 33)
-      return [(75+xPos)/2, (33+yPos)/2];
-
-    default: // left bottom @(25, 33)
-      return [(25+xPos)/2, ((33+yPos)/2)];
-  }
-}
-
-// generates the html of all the dots, which will be rendered in drawTriangle()
-function generateDots(quantity) {  
-  var dots = [];
-
-  //the three corners of the triangle
-  dots.push(<div id='top'></div>);
-  dots.push(<div id='right'></div>);
-  dots.push(<div id='left'></div>);
-
-  let point, xPos, yPos, setPoint;
-  point = calculateFirstPoint();
-
-  for (var i = 0; i < quantity; i++) {
-    if(i > 0) {
-      point = calculateNextPoint(point);
+    if(xPos >= 50) { //check which side of the triangle the point is on
+      return yPos < fr;
+    } else {
+      return yPos < fl;
     }
-    xPos = point[0].toFixed(2); // two decimal points for precision sake
-    yPos = point[1].toFixed(2);
-
-    setPoint = { // sets the location of the point
-      position: "absolute",
-      left: xPos+"vh",
-      bottom: yPos+"vh"
-    }
-    dots.push(<div className='dot' style={setPoint}></div>);
   }
-  return dots;
+
+  // generates the a random point within the area of the triangle
+  calculateFirstPoint() {
+    let xPos;
+    let yPos;
+    do {
+      xPos = Math.floor(Math.random() * 50) + 25;
+      yPos = Math.floor(Math.random() * 33) + 33;
+    } while (!this.pointIsValid(xPos, yPos));
+
+    return [xPos, yPos];
+  }
+
+  // calculates the next point to plot, ie the mid point between a randomly
+  // chosen corner point, and the previous plotted point
+  calculateNextPoint(point) {
+    let xPos = point[0];
+    let yPos = point[1];
+
+    // picks one of the three corners at random
+    let trianglePoint = Math.floor(Math.random() * 3) + 1;
+
+    // returns the midpoint between 'point' and the randomly chosen corner point
+    switch(trianglePoint) {
+      case 1: // top corner @(50, 66)
+        return [(50+xPos)/2, (66+yPos)/2];
+
+      case 2: // right bottom corner @(75, 33)
+        return [(75+xPos)/2, (33+yPos)/2];
+
+      default: // left bottom @(25, 33)
+        return [(25+xPos)/2, ((33+yPos)/2)];
+    }
+  }
+
+  // generates the html of all the dots, which will be rendered in drawTriangle()
+  generateDots(quantity) {  
+    var dots = [];
+
+    //the three corners of the triangle
+    dots.push(<div id='top'></div>);
+    dots.push(<div id='right'></div>);
+    dots.push(<div id='left'></div>);
+
+    let point, xPos, yPos, setPoint;
+    point = this.calculateFirstPoint();
+
+    for (var i = 0; i < quantity; i++) {
+      if(i > 0) {
+        point = this.calculateNextPoint(point);
+      }
+      xPos = point[0].toFixed(2); // two decimal points for precision sake
+      yPos = point[1].toFixed(2);
+
+      setPoint = { // sets the location of the point
+        position: "absolute",
+        left: xPos+"vh",
+        bottom: yPos+"vh"
+      }
+      dots.push(<div className='dot' style={setPoint}></div>);
+    }
+    return dots;
+  }
+
+  render() {
+    const quantity = this.props.quantity;
+    let key = 0;
+    return (
+      <div>
+        {
+          this.generateDots(quantity).map(dot => 
+            <div key={"dot" + key++}>{dot}</div> // React requires that each element of a list has a unique key
+          )
+        }
+      </div>
+    );
+  }
 }
 
 // this is the text form so the use can enter how many dots they want the
@@ -119,13 +129,15 @@ class NumDotsForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Enter the number of dots to be draw: 
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Draw" />
-      </form>
+      <div id="formContainer">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Enter the number of dots to be draw: 
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Draw" />
+        </form>
+      </div>
     );
   }
 }
